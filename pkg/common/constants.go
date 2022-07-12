2ee6ae80deaf3da1defc87f43417ea8a67211f66
ntishchauhan0022 <nitishchauhan0022@gmail.com>
2022-07-12 23:25:45 +0530

    add common package

---
+++ pkg/common/constants.go
@@ -0,0 +1,11 @@
+package common
+
+const(
+	AddonName = "otel-collector"
+	AddonFullName = "open-cluster-management:otel-collector"
+)
+
+const(
+	SubjectGroupOtelCollector      = "open-cluster-management:otel-collector"
+	SubjectUserClusterAddonAgent = "open-cluster-management:ote-collector:addon-agent"
+)
